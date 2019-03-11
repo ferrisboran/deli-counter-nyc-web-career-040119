b4bb30e8a1d214386828e0303b50d3ec7da03a83
Ferris Boran <ferris.boran@gmail.com>
2019-03-11 19:30:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(current_line)
+  x = "The line is currently"
+  peeps = []
+  current_line.each do |name|
+    peeps << "#{current_line.index(name)+1}. #{name}"
+  end
+  puts current_line == [] ? "#{x} empty." : "#{x}: #{peeps.join(' ')}"
+end
+
+def take_a_number(current_line,name)
+  name << current_line
+  puts current_line
+end
