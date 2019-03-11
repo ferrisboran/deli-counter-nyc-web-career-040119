c57b11a55cdebca4699999495ea47db45776d727
Ferris Boran <ferris.boran@gmail.com>
2019-03-11 19:32:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,17 @@
 def line(current_line)
   x = "The line is currently"
-  peeps = []
+  @peeps = []
   current_line.each do |name|
-    peeps << "#{current_line.index(name)+1}. #{name}"
+    @peeps << "#{current_line.index(name)+1}. #{name}"
   end
-  puts current_line == [] ? "#{x} empty." : "#{x}: #{peeps.join(' ')}"
+  puts current_line == [] ? "#{x} empty." : "#{x}: #{@peeps.join(' ')}"
 end
 
 def take_a_number(current_line,name)
   current_line.push(name)
   puts "Welcome, #{name}. You are number #{current_line.index(name) + 1} in line."
+end
+
+def now_serving
+
 end
